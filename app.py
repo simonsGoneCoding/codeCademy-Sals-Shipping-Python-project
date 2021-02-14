028be0dcf2bbcec29b5d954e554d17320a9546cc
Szymon Warszawa <szymonwarszawa@gmail.com>
2021-02-14 13:46:21 +0000

    Fix bugs.

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 # In this project, you’ll build a program that will take the weight of a package and determine the cheapest way to ship that package using Sal’s Shippers.
 
-weight = 8.4; 
+# weight = 4.8;  //ground is cheapest
+# weight = 2;  //drone is cheapest
+weight = 41.5;  # // premium is the cheapest
 
 #Ground Shipping: 
 
@@ -10,10 +12,10 @@ elif weight > 2 and weight <= 6:
   cost_ground = weight * 3.00 + 20.00; 
 elif weight > 6 and weight <= 10: 
   cost_ground = weight * 4.00 + 20.00; 
-elif weight > 10 
+elif weight > 10:
   cost_ground = weight * 4.75 + 20.00; 
 
-const_ground_premium = 125; 
+cost_ground_premium = 125; 
  
 print("Ground Shipping Premimium $", cost_ground_premium)
 print(cost_ground)    #returns 53.60
@@ -27,7 +29,8 @@ elif weight > 2 and weight <= 6:
   cost_drone = weight * 9.00; 
 elif weight > 6 and weight <= 10: 
   cost_drone = weight * 12.00; 
-elif weight > 10 
+elif weight > 10:
   cost_drone = weight * 14.25; 
 
+print(cost_drone)
 
